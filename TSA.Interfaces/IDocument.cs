099db84cda8b49d246041e4ffd7af78c81ae6351
Алексей Сенилов <thenn.erannor@gmail.com>
2018-12-15 11:10:50 +0300

    Проект для общих интерфейсов

---
+++ TSA.Interfaces/IDocument.cs
@@ -0,0 +1,8 @@
+﻿namespace TSA.Interfaces
+{
+    public interface IDocument
+    {
+        string Name { get; }
+        string Content { get; }
+    }
+}
---
+++ TSA.Interfaces/IDocumentSource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TSA.Interfaces
+{
+    public interface IDocumentSource
+    {
+        IEnumerable<IDocument> GetDocuments();
+    }
+}
