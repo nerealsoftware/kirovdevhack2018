c0ba08752448f1548b86cc6281667038ba2ba8d4
Aleksandr <alllllly@mail.ru>
2018-12-15 21:16:13 +0300

    Прототип

--- TSA.Web/Controllers/HomeController.cs
+++ TSA.Web/Controllers/HomeController.cs
@@ -2,29 +2,139 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TSA.Interfaces;
+using TSA.ML;
 using TSA.Web.Models;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
 
 namespace TSA.Web.Controllers
 {
+    public class Document : IDocument
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+
+    internal class JsonDocumentSource : IDocumentSource
+    {
+        public IEnumerable<IDocument> GetDocuments()
+        {
+            var docs = JsonConvert.DeserializeObject<List<Document>>(
+                File.ReadAllText(@"..\itmo.json"));
+            return docs;
+        }
+    }
+
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.step = "SelectSource";
             return View();
         }
 
-        [HttpGet]
-        public IActionResult ModelSelect(int source = -1)
+        [HttpPost]
+        public IActionResult Index(string step)
         {
-            if (source == -1) return RedirectToAction("Index");
+            if (step == "SelectSource") step = "SelectModel";
+            else if (step == "SelectModel") step = "Train";
+            else if (step == "Train")
+            {
+                step = "Results"; //step = "Topics";
+                if (topics != null)
+                {
+                    var sb = new StringBuilder();
+                    for (int i = 0; i < topics.Count; i++)
+                    {
+                        sb.AppendLine(string.Format("<h2> Категория номер {0} </h2>", i + 1));
+                        sb.AppendLine("<ul>");
+                        var counter = 0;
+                        foreach (var item in topics[i].Documents)
+                        {
+                            counter++;
+                            if (counter > 20)
+                            {
+                                sb.AppendLine(string.Format("<li>...</li>"));
+                                break;
+                            }
+                            sb.AppendLine(string.Format("<li>{0}</li>", System.Net.WebUtility.HtmlEncode(item.Name)));
+                        }
+                        sb.AppendLine("</ul>");
+                    }
+                    ViewBag.results = sb.ToString();
+                }
+            }
+            //else if (step == "Topics") step = "Results";
+            ViewBag.step = step;
             return View();
         }
 
-        public IActionResult Train()
+        protected static DateTime? lastStart = null;
+
+        [HttpPost]
+        public object StartTeach()
         {
+            string error = "";
+            lastStart = lastStart ?? DateTime.Now;
+            var thread = new Thread(TeachModel);
+            thread.Start();
+            return new { error = error, success = string.IsNullOrEmpty(error) };
+        }
+
+        private static IReadOnlyList<ITopic> topics;
+        private static object locker = new object();
+
+        public void TeachModel()
+        {
+            var topicGrouper = new TopicGrouper();
+            var tops = topicGrouper.GroupDocuments(new JsonDocumentSource(), 10);
+            lock (locker)
+            {
+                topics = tops;
+            }
+        }
+
+        [HttpPost]
+        public object IsTeachFinished()
+        {
+            string error = "";
+            var ls = lastStart;
+            var success = false;
+            lock (locker)
+            {
+                success = topics != null; // (ls != null) && (DateTime.Now.Subtract(ls.Value).TotalSeconds >= 45);
+            }
+            if (success) lastStart = null;
+            return new { success = success };
+        }
+
+        [HttpPost]
+        public IActionResult ModelSelect(MLModel model)
+        {
+            model.Step = MLModel.CurrentStep.SelectModel;
+            //if (source == -1) return RedirectToAction("Index");
+            return View(model);
+        }
+
+        public IActionResult Train(int source = -1, int model = -1)
+        {
+            if ((source == -1) && (model == -1)) return RedirectToAction("Index");
+            if (model == -1) return RedirectToAction("ModelSelect");
+            var thread = new Thread(Test);
+            thread.Start();
             return View();
+        }
+
+        public void Test()
+        {
+            Thread.Sleep(10000);
+            System.Diagnostics.Debug.WriteLine("Тест");
         }
 
         public IActionResult Topics()
---
+++ TSA.Web/Models/MLModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSA.Web.Models
+{
+    public class MLModel
+    {
+        public enum CurrentStep
+        {
+            SelectSource,
+            SelectModel,
+            Train,
+            Topics,
+            Results
+        }
+
+        public CurrentStep Step { get; set; }
+
+        public MLModel()
+        {
+            Step = CurrentStep.SelectSource;
+        }
+    }
+}
